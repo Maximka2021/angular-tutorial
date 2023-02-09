30eb3642327c0ffeb9e7b2b23da5fb90becef5a4
Maxim <polyakovmaxim05@gmail.com>
2023-02-09 09:59:53 -0500

    fetch

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {Task} from "../../Task"
-import {TASKS} from '../../mock-tasks'
+import {TaskService} from "../../services/task.service"
 
 @Component({
   selector: 'app-tasks',
@@ -8,5 +8,19 @@ import {TASKS} from '../../mock-tasks'
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent {
-  tasks: Task[] = TASKS
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void{
+    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks)
+  }
+
+  deleteTask(task: Task){
+    this.taskService.deleteTask(task).subscribe(() => (this.tasks = this.tasks.filter(t => t.id !== task.id)))
+  }
+
 }
+
+
+
